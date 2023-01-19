93ee504fc9a35e9c2fed9a08e8f6459c9f741b7b
shivamrishi98 <shivamrishisr@gmail.com>
2023-01-19 13:54:44 +1100

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Shivam Rishi on 19/01/23.
+//
+
+import Foundation
+
+/// Object that represents a singlet API call
+final class RMRequest {
+    
+}
---
+++ RickAndMorty/APIClient/RMService.swift
@@ -0,0 +1,26 @@
+//
+//  RMService.swift
+//  RickAndMorty
+//
+//  Created by Shivam Rishi on 19/01/23.
+//
+
+import Foundation
+
+/// Primary API service object to get Rick and Morty data
+final class RMService {
+    
+    /// Shared singleton instance
+    static let shared = RMService()
+    
+    /// Privatized constructor
+    private init() {}
+    
+    /// Send Rick and Morty API call
+    /// - Parameters:
+    ///   - request: Request instance
+    ///   - completion: Callback with data or error
+    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
+        
+    }
+}
