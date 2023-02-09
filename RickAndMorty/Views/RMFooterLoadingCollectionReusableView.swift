97b38fa3a527f5cee9668d18d8a7c9f70a617a58
shivamrishi98 <shivamrishisr@gmail.com>
2023-02-09 13:47:54 +1100

    Added pagination indicator

---
+++ RickAndMorty/Views/RMFooterLoadingCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  RMFooterLoadingCollectionReusableView.swift
+//  RickAndMorty
+//
+//  Created by Shivam Rishi on 9/2/2023.
+//
+
+import UIKit
+
+class RMFooterLoadingCollectionReusableView: UICollectionReusableView {
+        
+}
